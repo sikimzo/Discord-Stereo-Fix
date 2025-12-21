7dca7a04e02ed8ee7ad9219b6ff750636637bace
Sikimzo <khiaralaki9090@gmail.com>
2025-12-21 16:42:47 +0330

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -24,14 +24,12 @@ echo Select Your Discord For Installation
 echo [1] Discord
 echo [2] Discord PTB
 echo [3] Discord Canary
-echo [4] Uninstall Stereo
 echo.
 set /p choice=Enter Your Choice: 
 
 if "%choice%"=="1" set BASE=%LOCALAPPDATA%\Discord& set EXE=discord.exe& goto INSTALL
 if "%choice%"=="2" set BASE=%LOCALAPPDATA%\DiscordPTB& set EXE=discordptb.exe& goto INSTALL
 if "%choice%"=="3" set BASE=%LOCALAPPDATA%\DiscordCanary& set EXE=discordcanary.exe& goto INSTALL
-if "%choice%"=="4" goto UNINSTALL
 
 :: ================= INSTALL =================
 :INSTALL
@@ -75,38 +73,3 @@ if exist "%~dp0ffmpeg.dll" copy /Y "%~dp0ffmpeg.dll" "%FFMPEG%" >nul
 echo Installation completed
 timeout /t 3 /nobreak >NUL
 exit
-
-:: ================= UNINSTALL =================
-:UNINSTALL
-cls
-echo Select Discord To Uninstall Stereo From
-echo [1] Discord
-echo [2] Discord PTB
-echo [3] Discord Canary
-set /p uchoice=Choice: 
-
-if "%uchoice%"=="1" set BASE=%LOCALAPPDATA%\Discord& set EXE=discord.exe
-if "%uchoice%"=="2" set BASE=%LOCALAPPDATA%\DiscordPTB& set EXE=discordptb.exe
-if "%uchoice%"=="3" set BASE=%LOCALAPPDATA%\DiscordCanary& set EXE=discordcanary.exe
-
-taskkill /f /im %EXE% >nul 2>&1
-
-for /d %%A in ("%BASE%\app-*") do set APP=%%A
-for /d %%M in ("%APP%\modules\discord_voice-*") do set VOICE=%%M
-set TARGET=%VOICE%\discord_voice
-
-set TMP=%TEMP%\stereo_uninstall
-rmdir /s /q "%TMP%" >nul 2>&1
-mkdir "%TMP%"
-
-powershell -command ^
-"Invoke-WebRequest https://github.com/sikimzo/stereo-uninstall/archive/refs/heads/main.zip -OutFile '%TMP%\u.zip'"
-powershell -command ^
-"Expand-Archive '%TMP%\u.zip' '%TMP%'"
-
-xcopy "%TMP%\stereo-uninstall-main\*" "%TARGET%\" /E /H /Y >nul
-
-echo Stereo uninstalled
-timeout /t 3 /nobreak >NUL
-exit
-
