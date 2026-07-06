04288bbaa7bf90c342f9f8feb78d2d7cc77b81f4
Sikimzo <khiaralaki9090@gmail.com>
2026-07-06 13:19:07 +0330

    updated to v3.5.0

--- installer.bat
+++ installer.bat
@@ -1,9 +1,9 @@
 @echo off
 chcp 65001 >nul
-title SIKIMZO STEREO INSTALLER V3.4.2
+title SIKIMZO STEREO INSTALLER V3.5.0
 setlocal EnableDelayedExpansion
 
-set CURRENT_VERSION=V3.4.2
+set CURRENT_VERSION=V3.5.0
 
 :: ================ ANSI COLORS ================
 set ESC=
