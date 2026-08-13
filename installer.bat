41842165220560b92e66cd6e2b675a6e5fca1b85
Sikimzo <khiaralaki9090@gmail.com>
2026-08-14 03:27:52 +0330

    Update installer version to V3.6.1

--- installer.bat
+++ installer.bat
@@ -1,9 +1,9 @@
 @echo off
 chcp 65001 >nul
-title SIKIMZO STEREO INSTALLER V3.6.0
+title SIKIMZO STEREO INSTALLER V3.6.1
 setlocal EnableDelayedExpansion
 
-set CURRENT_VERSION=V3.6.0
+set CURRENT_VERSION=V3.6.1
 
 :: ================ ANSI COLORS ================
 set ESC=
