85c0dbe4a911000dcb36f8e834337646bad07812
Sikimzo <khiaralaki9090@gmail.com>
2026-08-25 04:48:42 +0330

    Update installer to version 3.7.0

--- installer.bat
+++ installer.bat
@@ -1,9 +1,9 @@
 @echo off
 chcp 65001 >nul
-title SIKIMZO STEREO INSTALLER V3.6.1
+title SIKIMZO STEREO INSTALLER V3.7.0
 setlocal EnableDelayedExpansion
 
-set CURRENT_VERSION=V3.6.1
+set CURRENT_VERSION=V3.7.0
 
 :: ================ ANSI COLORS ================
 set ESC=
